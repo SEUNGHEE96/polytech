356994952f4e80e39a2e11be981a15279daa0f3f
SEUNGHEE96 <dnffktmdgml@gmail.com>
2023-03-20 00:51:12 +0900

    refactor: 공사장 좌표를 그늘의 필드가 아닌 Judgement의 지역변수로 설정

--- CHECKPLACE/src/main/java/palce/domain/Judgement.java
+++ CHECKPLACE/src/main/java/palce/domain/Judgement.java
@@ -1,13 +1,27 @@
 package palce.domain;
 
+import palce.entity.ConstructionSite;
 import palce.entity.TreeShadow;
 import java.util.LinkedList;
 
 public class Judgement {
+    //공사장 객체 ConstructionSite를 Judge 클래스에 저장한 이유 :
+    //조용한 곳인지 시끄러운 곳인지 '판단'할 때에만 공사장 좌표가 필요하므로
+    //필요할때마다 호출하거나 매개변수로 받는 것보다 지역변수로 갖고 있는 것이 낫다고 판단
+    private ConstructionSite cs;
 
-    public boolean isSilent(TreeShadow ts) {
-        double distance = Math.pow((ts.getX() - ts.getConstructionSite().getA()), 2) + Math.pow((ts.getY() - ts.getConstructionSite().getB()), 2);
-        return (distance > Math.pow(ts.getConstructionSite().getR(), 2));
+    public void setConstructionSite(ConstructionSite cs) {
+        this.cs = cs;
+    }
+
+    enum Status {
+        silent, noisy
+    }
+    //isSilent 메소드는 Judgement 클래스에서만 쓰이므로 private으로 선언
+    private boolean isSilent(TreeShadow ts) {
+        double leftside = Math.pow((ts.getX() - cs.getX()), 2);
+        double rightside = Math.pow((ts.getY() - cs.getY()), 2);
+        return (leftside + rightside > Math.pow(cs.getR(), 2));
     }
 
     public String judgeAllTrees(LinkedList<TreeShadow> treelist) {
@@ -23,10 +37,6 @@ public class Judgement {
             }
         }
         return sb.toString();
-    }
-
-    enum Status {
-        silent, noisy
     }
 
 }
