c5a614918e287fa7a4ce463da56dcb89f7c16662
SEUNGHEE96 <dnffktmdgml@gmail.com>
2023-03-20 01:33:39 +0900

    feat: 테스트 코드 작성 시도

---
+++ CHECKPLACE/src/test/java/shadow/MainTest.java
@@ -0,0 +1,16 @@
+package shadow;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("테스트 코드 실행 :)");
+        System.out.println();
+    }
+
+}
