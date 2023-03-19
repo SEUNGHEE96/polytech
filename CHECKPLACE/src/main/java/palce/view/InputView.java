863870f5e93a6c89943061139177370c4c3a658b
SEUNGHEE96 <dnffktmdgml@gmail.com>
2023-03-19 23:51:07 +0900

    fix: 입력받는 view 클래스 구현

---
+++ CHECKPLACE/src/main/java/palce/view/InputView.java
@@ -0,0 +1,34 @@
+package palce.view;
+
+import palce.entity.ConstructionSite;
+import palce.entity.TreeShadow;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class InputView {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public ConstructionSite saveConstructionSite() {
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int R = scanner.nextInt();
+        return new ConstructionSite(a, b, R);
+    }
+
+    public int countTree() {
+        return scanner.nextInt();
+    }
+
+    public LinkedList<TreeShadow> saveTreeShadow(int cnt, ConstructionSite cs) {
+        LinkedList<TreeShadow> treelist = new LinkedList<>();
+        for (int i = 0; i < cnt; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            treelist.add(new TreeShadow(x, y, cs));
+        }
+        return treelist;
+    }
+
+}
