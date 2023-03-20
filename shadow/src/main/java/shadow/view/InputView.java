823a29aaa75a7b71b23851bff47032b8f5470b1e
SEUNGHEE LEE <108311729+SEUNGHEE96@users.noreply.github.com>
2023-03-20 10:56:34 +0900

    remove: 예외처리 삭제 (음수도 받을 수 있음)

--- shadow/src/main/java/shadow/view/InputView.java
+++ shadow/src/main/java/shadow/view/InputView.java
@@ -14,13 +14,11 @@ public class InputView {
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int R = scanner.nextInt();
-        validate(a, b, R);
         return new ConstructionSite(a, b, R);
     }
 
     public int countTree() {
         int cnt = scanner.nextInt();
-        validate(cnt);
         return cnt;
     }
 
@@ -29,19 +27,9 @@ public class InputView {
         for (int i = 0; i < cnt; i++) {
             int x = scanner.nextInt();
             int y = scanner.nextInt();
-            validate(x, y);
             treelist.add(new TreeShadow(x, y));
         }
         return treelist;
-    }
-
-    //Q. 예외처리는 어떤 클래스에서 하는 것이 가장 이상적인지...?
-    private void validate(int... nums) {
-        for(int n : nums) {
-            if(n < 0) {
-                throw new IllegalArgumentException("[ERROR] 유효한 값을 입력하세요!");
-            }
-        }
     }
 
 }
