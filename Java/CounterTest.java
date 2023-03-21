b97a0383fe07bc27df8d573412d0adc285b51289
SEUNGHEE LEE <108311729+SEUNGHEE96@users.noreply.github.com>
2023-03-21 16:42:13 +0900

    연습문제 10-3 수정

--- Java/CounterTest.java
+++ Java/CounterTest.java
@@ -5,19 +5,25 @@ import org.junit.Test;
 import com.example.Counter;
 
 public class CounterTest {
+    
+    @Test
+    public void 초기화_테스트() {
+        Counter c = new Counter();
+        assertEquals(c.getCount(), 0);
+    }
+    
+    @Test
+    public void 할당_테스트() {
+        Counter c = new Counter();
+        c.setCount(5);
+        assertEquals(c.getCount(), 5);
+    }
 
     @Test
     public void 증가_테스트() {
         Counter c = new Counter();
         c.increase();
         assertEquals(c.getCount(), 1);
-    }
-    
-    @Test
-    public void 감소_테스트() {
-        Counter c = new Counter();
-        c.decrease();
-        assertEquals(c.getCount(), -1);
     }
 
 }
