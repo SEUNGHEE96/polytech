0331870200a7717755964977d71759b1867d6f02
SEUNGHEE LEE <108311729+SEUNGHEE96@users.noreply.github.com>
2023-03-21 16:39:52 +0900

    연습문제 10-2 수정

---
+++ Java/DownCoutner.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class DownCoutner {
+    
+    private int count = 0;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void decrease() {
+        count--;
+    }
+
+}
