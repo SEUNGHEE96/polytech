2a5430aaa6cfa25cce65894dad9e03f9375f3b07
SEUNGHEE LEE <108311729+SEUNGHEE96@users.noreply.github.com>
2023-03-21 16:23:54 +0900

    연습문제 10-1 추가

--- Java/BankTest.java
+++ Java/BankTest.java
@@ -7,13 +7,10 @@ import com.example.Bank;
 
 public class BankTest {
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void test() {
-        //성공
-        Bank b = new Bank();
-        b.setName("김히어라");
-        assertEquals(b.getName(),"김히어라");
-        //실패
+        //예외가 발생하는 것을 원하는 코드는 해당 코드처럼 작성하면 된다!
+        //위에 expected를 명시해주면, 해당 예외가 발생한 것이 테스트 통과 처리가 된다!
         Bank b2 = new Bank();
         b2.setName("이승희");
         assertEquals(b2.getName(),"이승희");
