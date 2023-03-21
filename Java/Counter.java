57d1446d5cee19c4e1df52bb77c1dbe71b76cc29
SEUNGHEE LEE <108311729+SEUNGHEE96@users.noreply.github.com>
2023-03-21 16:34:25 +0900

    연습문제 10-2

---
+++ Java/Counter.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class Counter {
+
+    private int count = 0;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void increase() {
+        count++;
+    }
+
+    public void decrease() {
+        count--;
+    }
+}
