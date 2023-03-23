ad104fd896b7ffc2ed6133d1b6c43fdbd2c18aba
SEUNGHEE LEE <108311729+SEUNGHEE96@users.noreply.github.com>
2023-03-24 08:55:32 +0900

    add: 주석 추가

--- library/src/console/ErrorMessage.java
+++ library/src/console/ErrorMessage.java
@@ -2,4 +2,6 @@ package console;
 
 public enum ErrorMessage {
 
+	//시간 부족으로 미구현 ㅠㅠㅋㅋ
+	
 }
--- library/src/domain/Manager.java
+++ library/src/domain/Manager.java
@@ -11,14 +11,17 @@ public class Manager<E> {
 	Scanner sc = new Scanner(System.in);
 	InputView iv = new InputView();
 	MakeList ml = new MakeList();
+	//각 Entity의 list
 	List<E> list;
 	
+	//각 list 전체 조회
 	public void printAllList() {
 		for(E e : list) {
 			System.out.println(e);
 		}
 	}
-
+	
+	//하위 클래스에서 Manager에서 갖고있는 list를 가져오기 위한 메소드
 	public List<E> getList() {
 		return list;
 	}
